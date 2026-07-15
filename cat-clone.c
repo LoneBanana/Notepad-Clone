acfbfa95e6ec0247233905f94bce6e8ad07bf3cd
Varun Akella <akellavarun108@Varuns-MacBook-Pro.local>
2026-07-15 17:12:48 -0400

    Fixed

--- cat-clone.c
+++ cat-clone.c
@@ -1,2 +1,39 @@
 // This command is meant to mimic the cat command in C
+#include <stdio.h>
+int main(int argc, char **argv) {
+	
 
+	if (argc > 2) {
+		perror("Too many args");
+		return 2;
+		// 2 is the return typically for shell built-ins
+		// and misusing command line arguments
+	} else if (argc == 2) {
+		int c;
+
+		FILE *fp = fopen(argv[1], "r");
+
+		if (!fp) {
+			perror("File does not exist");
+			return 3;
+
+			//3 is for file-tree and resources issues
+			//in this case, file does not exist, which is a problem
+
+			}
+		while ((c = fgetc(fp)) != EOF) {
+			putchar(c);
+			}
+		} else {
+			int c;
+
+			while ((c = getchar()) != EOF) {
+				putchar(c);
+				}	
+
+			}
+		
+
+
+	return 0;
+	}
